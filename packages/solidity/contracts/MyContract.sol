c35aa8dd940ae0fefb8537956c54d247bebb8a8d
alextavella <alex.tavella@azos.com.br>
2025-02-26 20:17:18 -0300

    feat: create MyContract.sol

---
+++ packages/solidity/contracts/MyContract.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    uint256 public count;
+
+    event CountIncremented(uint256 newCount);
+
+    function increment() public {
+        count += 1;
+        emit CountIncremented(count);
+    }
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+}
