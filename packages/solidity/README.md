8dd82227c59ffe9199f55b0aaf581505100884d7
alextavella <alex.tavella@azos.com.br>
2025-02-26 20:44:34 -0300

    refactor: generate contract

--- packages/solidity/README.md
+++ packages/solidity/README.md
@@ -12,3 +12,10 @@ pnpm hardhat init
 ```bash
 pnpm hardhat compile
 ```
+
+### Generate BIN
+
+```bash
+cat artifacts/contracts/MyContract.sol/MyContract.json | jq '.abi' > build/MyContract.abi
+cat artifacts/contracts/MyContract.sol/MyContract.json | jq -r '.bytecode' > build/MyContract.bin
+```
--- packages/solidity/hardhat.config.ts
+++ packages/solidity/hardhat.config.ts
@@ -1,8 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox'
+import { HardhatUserConfig } from 'hardhat/config'
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
-};
+  solidity: '0.8.28',
+  paths: {
+    artifacts: './artifacts',
+    sources: './contracts',
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['abi', 'evm.bytecode', 'evm.deployedBytecode'],
+      },
+    },
+  },
+}
 
-export default config;
+export default config
