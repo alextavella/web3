de1ef6dde975677f2eb6260baff942131e628ed5
alextavella <alex.tavella@azos.com.br>
2025-02-26 20:44:53 -0300

    chore: backend

---
+++ packages/backend/pkg/contracts/mycontract.go
@@ -0,0 +1,420 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package contracts
+
+import (
+	"errors"
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = errors.New
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+	_ = abi.ConvertType
+)
+
+// ContractsMetaData contains all meta data concerning the Contracts contract.
+var ContractsMetaData = &bind.MetaData{
+	ABI: "[{\"anonymous\":false,\"inputs\":[{\"indexed\":false,\"internalType\":\"uint256\",\"name\":\"newCount\",\"type\":\"uint256\"}],\"name\":\"CountIncremented\",\"type\":\"event\"},{\"inputs\":[],\"name\":\"count\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"getCount\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"increment\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]",
+	Bin: "0x6080604052348015600f57600080fd5b506101bc8061001f6000396000f3fe608060405234801561001057600080fd5b50600436106100415760003560e01c806306661abd14610046578063a87d942c14610064578063d09de08a14610082575b600080fd5b61004e61008c565b60405161005b9190610108565b60405180910390f35b61006c610092565b6040516100799190610108565b60405180910390f35b61008a61009b565b005b60005481565b60008054905090565b60016000808282546100ad9190610152565b925050819055507f420680a649b45cbb7e97b24365d8ed81598dce543f2a2014d48fe328aa47e8bb6000546040516100e59190610108565b60405180910390a1565b6000819050919050565b610102816100ef565b82525050565b600060208201905061011d60008301846100f9565b92915050565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052601160045260246000fd5b600061015d826100ef565b9150610168836100ef565b92508282019050808211156101805761017f610123565b5b9291505056fea2646970667358221220fc1f92624904930db121fd631bf106e95552c7db4aeb3c7575e8b262cee88db464736f6c634300081c0033",
+}
+
+// ContractsABI is the input ABI used to generate the binding from.
+// Deprecated: Use ContractsMetaData.ABI instead.
+var ContractsABI = ContractsMetaData.ABI
+
+// ContractsBin is the compiled bytecode used for deploying new contracts.
+// Deprecated: Use ContractsMetaData.Bin instead.
+var ContractsBin = ContractsMetaData.Bin
+
+// DeployContracts deploys a new Ethereum contract, binding an instance of Contracts to it.
+func DeployContracts(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Contracts, error) {
+	parsed, err := ContractsMetaData.GetAbi()
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	if parsed == nil {
+		return common.Address{}, nil, nil, errors.New("GetABI returned nil")
+	}
+
+	address, tx, contract, err := bind.DeployContract(auth, *parsed, common.FromHex(ContractsBin), backend)
+	if err != nil {
+		return common.Address{}, nil, nil, err
+	}
+	return address, tx, &Contracts{ContractsCaller: ContractsCaller{contract: contract}, ContractsTransactor: ContractsTransactor{contract: contract}, ContractsFilterer: ContractsFilterer{contract: contract}}, nil
+}
+
+// Contracts is an auto generated Go binding around an Ethereum contract.
+type Contracts struct {
+	ContractsCaller     // Read-only binding to the contract
+	ContractsTransactor // Write-only binding to the contract
+	ContractsFilterer   // Log filterer for contract events
+}
+
+// ContractsCaller is an auto generated read-only Go binding around an Ethereum contract.
+type ContractsCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ContractsTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type ContractsTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ContractsFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type ContractsFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ContractsSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type ContractsSession struct {
+	Contract     *Contracts        // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// ContractsCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type ContractsCallerSession struct {
+	Contract *ContractsCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts    // Call options to use throughout this session
+}
+
+// ContractsTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type ContractsTransactorSession struct {
+	Contract     *ContractsTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts    // Transaction auth options to use throughout this session
+}
+
+// ContractsRaw is an auto generated low-level Go binding around an Ethereum contract.
+type ContractsRaw struct {
+	Contract *Contracts // Generic contract binding to access the raw methods on
+}
+
+// ContractsCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type ContractsCallerRaw struct {
+	Contract *ContractsCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// ContractsTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type ContractsTransactorRaw struct {
+	Contract *ContractsTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewContracts creates a new instance of Contracts, bound to a specific deployed contract.
+func NewContracts(address common.Address, backend bind.ContractBackend) (*Contracts, error) {
+	contract, err := bindContracts(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Contracts{ContractsCaller: ContractsCaller{contract: contract}, ContractsTransactor: ContractsTransactor{contract: contract}, ContractsFilterer: ContractsFilterer{contract: contract}}, nil
+}
+
+// NewContractsCaller creates a new read-only instance of Contracts, bound to a specific deployed contract.
+func NewContractsCaller(address common.Address, caller bind.ContractCaller) (*ContractsCaller, error) {
+	contract, err := bindContracts(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &ContractsCaller{contract: contract}, nil
+}
+
+// NewContractsTransactor creates a new write-only instance of Contracts, bound to a specific deployed contract.
+func NewContractsTransactor(address common.Address, transactor bind.ContractTransactor) (*ContractsTransactor, error) {
+	contract, err := bindContracts(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &ContractsTransactor{contract: contract}, nil
+}
+
+// NewContractsFilterer creates a new log filterer instance of Contracts, bound to a specific deployed contract.
+func NewContractsFilterer(address common.Address, filterer bind.ContractFilterer) (*ContractsFilterer, error) {
+	contract, err := bindContracts(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &ContractsFilterer{contract: contract}, nil
+}
+
+// bindContracts binds a generic wrapper to an already deployed contract.
+func bindContracts(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := ContractsMetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Contracts *ContractsRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Contracts.Contract.ContractsCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Contracts *ContractsRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Contracts.Contract.ContractsTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Contracts *ContractsRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Contracts.Contract.ContractsTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Contracts *ContractsCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Contracts.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Contracts *ContractsTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Contracts.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Contracts *ContractsTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Contracts.Contract.contract.Transact(opts, method, params...)
+}
+
+// Count is a free data retrieval call binding the contract method 0x06661abd.
+//
+// Solidity: function count() view returns(uint256)
+func (_Contracts *ContractsCaller) Count(opts *bind.CallOpts) (*big.Int, error) {
+	var out []interface{}
+	err := _Contracts.contract.Call(opts, &out, "count")
+
+	if err != nil {
+		return *new(*big.Int), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new(*big.Int)).(**big.Int)
+
+	return out0, err
+
+}
+
+// Count is a free data retrieval call binding the contract method 0x06661abd.
+//
+// Solidity: function count() view returns(uint256)
+func (_Contracts *ContractsSession) Count() (*big.Int, error) {
+	return _Contracts.Contract.Count(&_Contracts.CallOpts)
+}
+
+// Count is a free data retrieval call binding the contract method 0x06661abd.
+//
+// Solidity: function count() view returns(uint256)
+func (_Contracts *ContractsCallerSession) Count() (*big.Int, error) {
+	return _Contracts.Contract.Count(&_Contracts.CallOpts)
+}
+
+// GetCount is a free data retrieval call binding the contract method 0xa87d942c.
+//
+// Solidity: function getCount() view returns(uint256)
+func (_Contracts *ContractsCaller) GetCount(opts *bind.CallOpts) (*big.Int, error) {
+	var out []interface{}
+	err := _Contracts.contract.Call(opts, &out, "getCount")
+
+	if err != nil {
+		return *new(*big.Int), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new(*big.Int)).(**big.Int)
+
+	return out0, err
+
+}
+
+// GetCount is a free data retrieval call binding the contract method 0xa87d942c.
+//
+// Solidity: function getCount() view returns(uint256)
+func (_Contracts *ContractsSession) GetCount() (*big.Int, error) {
+	return _Contracts.Contract.GetCount(&_Contracts.CallOpts)
+}
+
+// GetCount is a free data retrieval call binding the contract method 0xa87d942c.
+//
+// Solidity: function getCount() view returns(uint256)
+func (_Contracts *ContractsCallerSession) GetCount() (*big.Int, error) {
+	return _Contracts.Contract.GetCount(&_Contracts.CallOpts)
+}
+
+// Increment is a paid mutator transaction binding the contract method 0xd09de08a.
+//
+// Solidity: function increment() returns()
+func (_Contracts *ContractsTransactor) Increment(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Contracts.contract.Transact(opts, "increment")
+}
+
+// Increment is a paid mutator transaction binding the contract method 0xd09de08a.
+//
+// Solidity: function increment() returns()
+func (_Contracts *ContractsSession) Increment() (*types.Transaction, error) {
+	return _Contracts.Contract.Increment(&_Contracts.TransactOpts)
+}
+
+// Increment is a paid mutator transaction binding the contract method 0xd09de08a.
+//
+// Solidity: function increment() returns()
+func (_Contracts *ContractsTransactorSession) Increment() (*types.Transaction, error) {
+	return _Contracts.Contract.Increment(&_Contracts.TransactOpts)
+}
+
+// ContractsCountIncrementedIterator is returned from FilterCountIncremented and is used to iterate over the raw logs and unpacked data for CountIncremented events raised by the Contracts contract.
+type ContractsCountIncrementedIterator struct {
+	Event *ContractsCountIncremented // Event containing the contract specifics and raw log
+
+	contract *bind.BoundContract // Generic contract to use for unpacking event data
+	event    string              // Event name to use for unpacking event data
+
+	logs chan types.Log        // Log channel receiving the found contract events
+	sub  ethereum.Subscription // Subscription for errors, completion and termination
+	done bool                  // Whether the subscription completed delivering logs
+	fail error                 // Occurred error to stop iteration
+}
+
+// Next advances the iterator to the subsequent event, returning whether there
+// are any more events found. In case of a retrieval or parsing error, false is
+// returned and Error() can be queried for the exact failure.
+func (it *ContractsCountIncrementedIterator) Next() bool {
+	// If the iterator failed, stop iterating
+	if it.fail != nil {
+		return false
+	}
+	// If the iterator completed, deliver directly whatever's available
+	if it.done {
+		select {
+		case log := <-it.logs:
+			it.Event = new(ContractsCountIncremented)
+			if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+				it.fail = err
+				return false
+			}
+			it.Event.Raw = log
+			return true
+
+		default:
+			return false
+		}
+	}
+	// Iterator still in progress, wait for either a data or an error event
+	select {
+	case log := <-it.logs:
+		it.Event = new(ContractsCountIncremented)
+		if err := it.contract.UnpackLog(it.Event, it.event, log); err != nil {
+			it.fail = err
+			return false
+		}
+		it.Event.Raw = log
+		return true
+
+	case err := <-it.sub.Err():
+		it.done = true
+		it.fail = err
+		return it.Next()
+	}
+}
+
+// Error returns any retrieval or parsing error occurred during filtering.
+func (it *ContractsCountIncrementedIterator) Error() error {
+	return it.fail
+}
+
+// Close terminates the iteration process, releasing any pending underlying
+// resources.
+func (it *ContractsCountIncrementedIterator) Close() error {
+	it.sub.Unsubscribe()
+	return nil
+}
+
+// ContractsCountIncremented represents a CountIncremented event raised by the Contracts contract.
+type ContractsCountIncremented struct {
+	NewCount *big.Int
+	Raw      types.Log // Blockchain specific contextual infos
+}
+
+// FilterCountIncremented is a free log retrieval operation binding the contract event 0x420680a649b45cbb7e97b24365d8ed81598dce543f2a2014d48fe328aa47e8bb.
+//
+// Solidity: event CountIncremented(uint256 newCount)
+func (_Contracts *ContractsFilterer) FilterCountIncremented(opts *bind.FilterOpts) (*ContractsCountIncrementedIterator, error) {
+
+	logs, sub, err := _Contracts.contract.FilterLogs(opts, "CountIncremented")
+	if err != nil {
+		return nil, err
+	}
+	return &ContractsCountIncrementedIterator{contract: _Contracts.contract, event: "CountIncremented", logs: logs, sub: sub}, nil
+}
+
+// WatchCountIncremented is a free log subscription operation binding the contract event 0x420680a649b45cbb7e97b24365d8ed81598dce543f2a2014d48fe328aa47e8bb.
+//
+// Solidity: event CountIncremented(uint256 newCount)
+func (_Contracts *ContractsFilterer) WatchCountIncremented(opts *bind.WatchOpts, sink chan<- *ContractsCountIncremented) (event.Subscription, error) {
+
+	logs, sub, err := _Contracts.contract.WatchLogs(opts, "CountIncremented")
+	if err != nil {
+		return nil, err
+	}
+	return event.NewSubscription(func(quit <-chan struct{}) error {
+		defer sub.Unsubscribe()
+		for {
+			select {
+			case log := <-logs:
+				// New log arrived, parse the event and forward to the user
+				event := new(ContractsCountIncremented)
+				if err := _Contracts.contract.UnpackLog(event, "CountIncremented", log); err != nil {
+					return err
+				}
+				event.Raw = log
+
+				select {
+				case sink <- event:
+				case err := <-sub.Err():
+					return err
+				case <-quit:
+					return nil
+				}
+			case err := <-sub.Err():
+				return err
+			case <-quit:
+				return nil
+			}
+		}
+	}), nil
+}
+
+// ParseCountIncremented is a log parse operation binding the contract event 0x420680a649b45cbb7e97b24365d8ed81598dce543f2a2014d48fe328aa47e8bb.
+//
+// Solidity: event CountIncremented(uint256 newCount)
+func (_Contracts *ContractsFilterer) ParseCountIncremented(log types.Log) (*ContractsCountIncremented, error) {
+	event := new(ContractsCountIncremented)
+	if err := _Contracts.contract.UnpackLog(event, "CountIncremented", log); err != nil {
+		return nil, err
+	}
+	event.Raw = log
+	return event, nil
+}
